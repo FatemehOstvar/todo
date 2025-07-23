34d7679d04bfeccf2ec749a22659f3bea6bb1afc
FatemehOstvar <fatemehostvar2005@gmail.com>
2025-07-23 14:31:48 +0330

    merged from master

--- app/database.py
+++ app/database.py
@@ -7,6 +7,9 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL","sqlite:///./default.db")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
